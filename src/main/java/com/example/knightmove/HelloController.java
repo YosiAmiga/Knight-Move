99e4028070512ccdfd6a3d129c746f92fab3d748
Yosi Amiga <yosiamiga1710@gmail.com>
2022-10-31 20:18:41 +0200

    Testing new branch

--- src/main/java/com/example/knightmove/HelloController.java
+++ src/main/java/com/example/knightmove/HelloController.java
@@ -2,13 +2,17 @@ package com.example.knightmove;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.text.Text;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
 
     @FXML
+    private Text branchTest;
+    @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+        branchTest.setText("Testing new branch creation!");
     }
 }
