ef4a5e673a21ba436320e4fd01f19ace896f1291
Naor Barzilay <naorbarzilay1@gmail.com>
2022-11-24 18:15:37 +0200

    add timer and change level

--- src/main/java/com/example/knightmove/controllers/GamePageController.java
+++ src/main/java/com/example/knightmove/controllers/GamePageController.java
@@ -2,16 +2,23 @@ package com.example.knightmove.controllers;
 
 import com.example.knightmove.HelloApplication;
 import com.example.knightmove.Model.Game;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.io.IOException;
 
@@ -21,18 +28,59 @@ public class GamePageController {
     private Scene scene;
     private Parent root;
 
-    public void initialize(){
+    private Timeline timeline = new Timeline();
+
+    private int startTimeSec;
+
+
+    public void initialize() {
 
         // Themes are Coral, Dusk, Wheat, Marine, Emerald, Sandcastle
         Game game = new Game(chessBoard, "Sandcastle");
+        startTimeSec = 10; // Change to 60!
 
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.getKeyFrames().add(new KeyFrame(Duration.seconds(1), new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                startTimeSec--;
+                boolean isSecondsZero = startTimeSec == 0;
+                boolean timeToChangeLevel = startTimeSec == 0;
+
+                if (isSecondsZero) {
+                    startTimeSec = 10;
+                }
+                if (timeToChangeLevel) {
+                    timeline.stop();
+                    startTimeSec = 10;
+                    if(currentLevelText.getText().equals("1")){
+                        currentLevelText.setText("2");
+                    }
+                    else if(currentLevelText.getText().equals("2")){
+                        currentLevelText.setText("3");
+                    }
+                    else if(currentLevelText.getText().equals("3")){
+                        currentLevelText.setText("4");
+                    }
+                }
+
+                currentTimeText.setText(String.format("%02d sec", startTimeSec));
+
+            }
+        }));
+        timeline.playFromStart();
     }
+
     public void returnToAppIntroPage(ActionEvent event) throws IOException {
         root = FXMLLoader.load(HelloApplication.class.getResource("AppIntroPage.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+    }
+
+    public void newLevel(ActionEvent event) {
+        this.initialize();
     }
 
     @FXML
@@ -49,5 +97,11 @@ public class GamePageController {
 
     @FXML
     private ImageView boardCurrentStateImage;
+
+    @FXML
+    private Label currentLevelText;
+
+    @FXML
+    private Label currentTimeText;
 
 }
