8e05665d05b27fb40be56137fdcd5cbaa38bf065
itaykarat <itaykarat13@gmail.com>
2022-11-25 16:56:48 +0200

    full screen view

--- src/main/java/com/example/knightmove/controllers/LogInController.java
+++ src/main/java/com/example/knightmove/controllers/LogInController.java
@@ -68,7 +68,7 @@ public class LogInController {
                 stage = (Stage)((Node)event.getSource()).getScene().getWindow();
                 scene = new Scene(root);
                 stage.setScene(scene);
-                stage.setFullScreen(true); // set full screen
+                stage.setFullScreen(true); // set full scree
                 stage.show();
             }
         }
