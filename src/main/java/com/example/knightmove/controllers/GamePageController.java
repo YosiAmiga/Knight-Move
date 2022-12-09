2bbe4ad2750522e45c0d41674a21245caef1ca8b
Yosi Amiga <yosiamiga1710@gmail.com>
2022-12-09 10:48:40 +0200

    Created a question popup and selecting an answer, showing correct or not

--- src/main/java/com/example/knightmove/controllers/GamePageController.java
+++ src/main/java/com/example/knightmove/controllers/GamePageController.java
@@ -1,6 +1,8 @@
 package com.example.knightmove.controllers;
 
 import com.example.knightmove.HelloApplication;
+import com.example.knightmove.Model.Question;
+import javafx.scene.control.ButtonType;
 import javafx.scene.layout.Pane;
 import javafx.stage.Modality;
 import com.example.knightmove.Model.Game;
@@ -23,8 +25,17 @@ import javafx.stage.Popup;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
+import java.io.FileReader;
 import java.io.IOException;
-
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.concurrent.ThreadLocalRandom;
+// import the required classes
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import static com.example.knightmove.Model.Json.readFromJSON;
 import static java.lang.System.exit;
 
 public class GamePageController {
@@ -37,8 +48,12 @@ public class GamePageController {
 
     private int startTimeSec;
 
+    public GamePageController() throws IOException, ParseException {
+    }
+
 
     public void initialize() {
+        createQuestionPopUp();
 
         // Themes are Coral, Dusk, Wheat, Marine, Emerald, Sandcastle
         Game game = new Game(chessBoard, "Sandcastle");
@@ -149,5 +164,48 @@ public class GamePageController {
 
     @FXML
     private Label currentTimeText;
+
+    public void createQuestionPopUp(){
+        HashSet<Question> allQuestionsInJSON= readFromJSON();
+        // convert the HashSet to an array
+        Object[] array = allQuestionsInJSON.toArray();
+        // get a random questions from the array
+        Object randomQuestion = array[ThreadLocalRandom.current().nextInt(array.length)];
+        Question questionObject = (Question) randomQuestion;
+        String questionNameToPopUp = questionObject.getQuestion();
+        ArrayList<String> answers = questionObject.getAnswers();
+        ArrayList<ButtonType> answersOptions = new ArrayList<>();
+        for(String answer : answers){
+            answersOptions.add(new ButtonType(answer));
+        }
+        Integer correctAnswerNumber = questionObject.getCorrectAnswer();
+        String correctAnswerStringByIndex = answers.get(correctAnswerNumber-1);
+        
+        // create an Alert object
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION,"nevermind ",answersOptions.get(0),answersOptions.get(1),answersOptions.get(2),answersOptions.get(3));
+        alert.setHeaderText(questionNameToPopUp);
+        // set the alert's message to the first question
+        alert.setContentText("Select your answer:");
+        // show the alert and get the user's response
+        ButtonType response = alert.showAndWait().orElse(null);
+        String playerSelectedAnswer = response.getText();
+
+        Alert correctAnswer = new Alert(Alert.AlertType.INFORMATION);
+        correctAnswer.setTitle("Correct Answer");
+        correctAnswer.setHeaderText("Correct Answer");
+        correctAnswer.setContentText("Congratulations, that is the correct answer.");
+
+        Alert wrongAnswer = new Alert(Alert.AlertType.ERROR);
+        wrongAnswer.setTitle("Wrong Answer");
+        wrongAnswer.setHeaderText("Wrong Answer");
+        wrongAnswer.setContentText("Sorry, that is the wrong answer. Please try again.");
+
+        // check the user's response
+        if (playerSelectedAnswer.equals(correctAnswerStringByIndex)) {
+            correctAnswer.showAndWait();
+        }else {
+            wrongAnswer.showAndWait();
+        }
+    }
 
 }
