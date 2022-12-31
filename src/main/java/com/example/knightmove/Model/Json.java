855129c9f63805b4dec7c4f8ba26ed7e77d66f30
Dana412 <dana.zidon@gmail.com>
2022-12-31 12:23:58 +0200

    Fixed edit question and deleting unwanted printing

--- src/main/java/com/example/knightmove/Model/Json.java
+++ src/main/java/com/example/knightmove/Model/Json.java
@@ -38,10 +38,6 @@ public class Json {
                 String team = (String)jsonObject2.get("team");
                 String level = jsonObject2.get("level").toString();
                 String correctans = jsonObject2.get("correct_ans").toString();
-//                System.out.println("question " + q);
-//                System.out.println("answers " + answers);
-//                System.out.println("team " + team);
-//                System.out.println("level " + level);
                 Question newQuestion = new Question(q,answers,Integer.valueOf(correctans),Integer.valueOf(level),team);
                 questions.add(newQuestion);
             }
@@ -55,47 +51,6 @@ public class Json {
         return questions;
     }
 
-
-    public static void writeToJson(Question q){
-
-        JSONObject newQuestion = new JSONObject();
-        newQuestion.put("question",q.getQuestion());
-        JSONArray answersArray = new JSONArray();
-        for (String answer :q.getAnswers()) {
-            answersArray.add(answer);
-        }
-        newQuestion.put("answers",answersArray);
-        newQuestion.put("correct_ans",q.getCorrectAnswer());
-        newQuestion.put("level",q.getLevel());
-        newQuestion.put("team",q.getTeam());
-
-
-        try(FileWriter file = new FileWriter("src/Questions.json")){
-            JSONObject doc = new JSONObject();
-            JSONArray questionArray = new JSONArray();
-            JSONObject questObj = new JSONObject();
-            for (Question question1 :HelloApplication.s.getQuestions()){
-                JSONArray answersArray2 = new JSONArray();
-                questObj.put("question", question1.getQuestion());
-                for (String answer :question1.getAnswers()) {
-                    answersArray2.add(answer);
-                }
-                questObj.put("answers", answersArray2);
-                questObj.put("correct_ans", question1.getCorrectAnswer());
-                questObj.put("level", question1.getLevel());
-                questObj.put("team", question1.getTeam());
-                questionArray.add(questObj);
-                System.out.println(question1);
-
-            }
-            questionArray.add(newQuestion);
-            doc.put("questions",questionArray);
-            file.write(doc.toJSONString());
-            file.flush();
-        }catch (IOException e){e.printStackTrace();}
-
-}
-
         public static void updateJson(){
 
             try(FileWriter file = new FileWriter("src/Questions.json")){
@@ -104,8 +59,6 @@ public class Json {
 
                 for (Question question1 :HelloApplication.s.getQuestions()){
                     JSONObject questObj = new JSONObject();
-                    System.out.println("start writing to json");
-                    System.out.println("The Question " + question1);
                     JSONArray answersArray2 = new JSONArray();
                     questObj.put("question", question1.getQuestion());
                     for (String answer :question1.getAnswers()) {
